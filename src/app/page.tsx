db48bbc9aababfd9fcaa78643b7b8282ec9f2ee4
ismael strey pereira <ismaelstrey@hotmail.com>
2024-04-10 23:16:48 -0300

    header site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,20 @@ import { Navgator } from "@/components/header";
 export default function Home() {
   return (
     <div className="bg-image-mulher bg-no-repeat bg-cover">
-      <main className="flex min-h-screen flex-col items-center justify-between bg-white/80">
+      <main className="flex flex-col items-center justify-between bg-white/80">
         <Navgator />
-        <article className="flex w-full">
+        <article className="flex w-full relative top-0">
           <video
             autoPlay
             loop
             muted
-            className="absolute -top-28 z-10 w-full max-w-none"
+            className="absolute -top-28 z-10 w-full max-w-none justify-between"
           >
             <source height={"50px"} src="video.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
+          <div className="flex end-0 z-50 bg-primary absolute w-full h-2.5"></div>
         </article>
-        <div className="z-50 bg-red-500">
-          {" "}
-          <span>teste</span>
-          <span>teste</span>
-          <span>teste</span>
-          <span>teste</span>
-          <span>teste</span>
-          <span>teste</span>
-        </div>
       </main>
     </div>
   );
