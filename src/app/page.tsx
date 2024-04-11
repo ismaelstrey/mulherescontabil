13e2ca4693668a9576f35abf1e2c809d0ac5ce66
ismael strey pereira <ismaelstrey@hotmail.com>
2024-04-10 23:01:33 -0300

    header site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
+import { Navgator } from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <article className="flex w-screen h-screen bg-image-mulher bg-no-repeat bg-cover justify-center items-center">
-        <div className="flex text-8xl text-emerald-400">Site em construção</div>
-      </article>
-    </main>
+    <div className="bg-image-mulher bg-no-repeat bg-cover">
+      <main className="flex min-h-screen flex-col items-center justify-between bg-white/80">
+        <Navgator />
+        <article className="flex w-full">
+          <video
+            autoPlay
+            loop
+            muted
+            className="absolute -top-28 z-10 w-full max-w-none"
+          >
+            <source height={"50px"} src="video.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </article>
+        <div className="z-50 bg-red-500">
+          {" "}
+          <span>teste</span>
+          <span>teste</span>
+          <span>teste</span>
+          <span>teste</span>
+          <span>teste</span>
+          <span>teste</span>
+        </div>
+      </main>
+    </div>
   );
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React, { ReactNode } from "react";
+import Logo from "../icons/logo";
+interface PropsItem {
+  href: string;
+  children: ReactNode;
+}
+
+function NavItem(props: PropsItem) {
+  return (
+    <li className="hover:border-b-4 border-primary pb-1">
+      <Link {...props} />
+    </li>
+  );
+}
+
+function Navgator() {
+  return (
+    <>
+      {" "}
+      <div className="flex z-20 h-20 bg-zinc-800 w-full text-white justify-around">
+        <div className="flex content-start items-center">
+          <div className="flex gap-3">
+            <Logo />
+            <span className="text-2xl mt-2 text-primary">
+              Mulheres contábil
+            </span>
+          </div>
+        </div>
+        <ul className="flex gap-8 justify-center items-center">
+          <NavItem href="#quem_somos">Quem somos</NavItem>
+          <NavItem href="#solucoes">Soluçoes</NavItem>
+          <NavItem href="#encontros">Encontros</NavItem>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export { Navgator };
